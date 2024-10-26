7438adf7ec53c3d906db380b928d1cb4949db8be
NguyenBaCuong532 <cuongmja532@gmail.com>
2024-10-26 12:00:41 +0700

    test project

---
+++ auth/dangky.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Shop | Đăng ký</title>
+    <link rel="stylesheet" href="dangky.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+		if (isset($_POST["btn_submit"])) {
+  			//lấy thông tin từ các form bằng phương thức POST
+  			$username = $_POST["username"];
+  			$name = $_POST["name"];
+
+  			$password = $_POST["password"];
+  			$rpassword = $_POST["rpassword"];
+
+ 			$email = $_POST["email"];
+            $diachi = $_POST["diachi"];
+			$sdt = $_POST["sdt"];
+
+  			
+  			//Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
+			  if (strcmp($password ,$rpassword)!=0) {
+				   echo "Mật Khẩu Không Khớp!";
+  			}else{
+  					// Kiểm tra tài khoản đã tồn tại chưa
+					$conn = mysqli_connect("localhost", "root", "", "dawtmdt_phukienthoitrang");
+  					$sql="SELECT * from user where Username='$username'";
+					$kt=mysqli_query($conn, $sql);
+
+					if(mysqli_num_rows($kt)  > 0){
+						echo "Tài khoản đã tồn tại";
+					}else{
+						//thực hiện việc lưu trữ dữ liệu vào db
+	    				$sql = "INSERT INTO user (Name,Username,Email,Password,Address,Phone) VALUES ('$name','$username','$email','$password','$diachi','$sdt')";
+					    // thực thi câu $sql với biến conn lấy từ file connection.php
+   						mysqli_query($conn,$sql);
+				   		echo "<p>chúc mừng bạn đã đăng ký thành công</p>";
+					}
+			  }
+	}
+	?>
+    <div class="wrapper">
+        <div class="title-text">
+            
+            <div class="title signup">
+               Đăng Ký
+            </div>
+        </div>
+
+        <form action="" method="POST" class="signup">
+            <div class="input-group">
+                <input type="text" name="name" id="name" required>
+                <label htmlFor=''>Tên người dùng</label>
+
+            </div>
+            <div class="input-group">
+                <input type="email" name="email" id="email" required>
+                <label htmlFor=''>Email</label>
+
+            </div>
+            <div class="input-group">
+                <input type="text" name="diachi" id="diachi" required>
+                <label htmlFor=''>Địa chỉ</label>
+
+            </div>
+            <div class="input-group">
+                <input type="text" name="sdt" id="sdt" required>
+                <label htmlFor=''>Số điện thoại</label>
+
+            </div>
+            <div class="input-group">
+                <input type="text" name="username" id="username" required>
+                <label htmlFor=''>Tên tài Khoản</label>
+
+            </div>
+            <div class="input-group">
+                <input type="text" name="password" id="password" required>
+                <label htmlFor=''>Mật khẩu</label>
+
+            </div>
+            <div class="input-group">
+                <input type="text" name="rpassword" id="rpassword" required>
+                <label htmlFor=''>Nhập lại mật khẩu</label>
+
+            </div>
+            <div class="input-group btn">
+                <div class="btn-layer"></div>
+                <input type="submit" name="btn_submit" class="submit" value="Đăng Ký">
+            </div>
+            <div class="signup-link">
+                 <a href="./dangnhap.php">Đăng nhập tại đây.</a>
+               </div>
+        </form>
+    </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
---
+++ auth/dangnhap.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+
+<head>
+   <meta charset="utf-8">
+   <title>Shop | Đăng Nhập</title>
+   <link rel="stylesheet" href="dangnhap.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+
+<body>
+   <?php
+
+   // Kiểm tra nếu người dùng đã ân nút đăng nhập thì mới xử lý
+   if (isset($_POST["btns_submit"])) {
+      // lấy thông tin người dùng
+      $username = $_POST["username"];
+      $password = $_POST["password"];
+      //mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
+
+      if ($username == "" || $password == "") {
+         echo "<p>username hoặc password bạn không được để trống!</p>";
+      } else {
+         $conn = mysqli_connect("localhost", "root", "", "dawtmdt_phukienthoitrang");
+         $sql = "SELECT * from user where Username = '$username' AND Password = '$password' ";
+         $query = mysqli_query($conn, $sql);
+         $row = mysqli_fetch_array($query);
+         if ($row == 0) {
+            echo "<p>Tên đăng nhập hoặc mật khẩu không đúng !</p>";
+         } else {
+            //tiến hành lưu tên đăng nhập vào session để tiện xử lý sau này
+            $_SESSION['username'] = $username;
+            $_SESSION['id'] = $row['Id'];
+            $_SESSION['hoten'] = $row['Name'];
+            $_SESSION['diachi'] = $row['Address'];
+            $_SESSION['sdt'] = $row['Phone'];
+            $_SESSION['password'] = $password;
+
+            // Thực thi hành động sau khi lưu thông tin vào session
+            // ở đây tiến hành chuyển hướng trang web tới một trang gọi là index.php
+            header('Location: ../trangchu.php');
+            mysqli_close($conn);
+         }
+      }
+   }
+   ?>
+
+   <div class="wrapper">
+      <div class="title-text">
+         <div class="title login">
+            Đăng Nhập
+         </div>
+         <div class="title signup">
+            Đăng Ký
+         </div>
+      </div>
+      <div class="form-container">
+        
+         <div class="form-inner">
+            <form action="" method="POST" class="login">
+               <div class="input-group">
+                  <input type="text" name="username" id="username" required>
+                  <label htmlFor=''>Tên tài Khoản</label>
+
+               </div>
+               <div class="input-group">
+                  <input type="password" name="password" id="password" required>
+                  <label htmlFor=''>Mật khẩu</label>
+
+               </div>
+               <div class="pass-link">
+                  <a href="./ngu.php">Quên mật khẩu ?</a>
+               </div>
+               <div class="input-group btn">
+                  <div class="btn-layer"></div>
+                  <input type="submit" name="btns_submit" class="submit" value="Đăng Nhập">
+               </div>
+               <div class="signup-link">
+                  Nhớ mật khẩu? <a href="./dangky.php">Đăng ký tại đây.</a>
+               </div>
+            </form>
+</body>
+
+</html>
